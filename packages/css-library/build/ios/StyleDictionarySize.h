d4ced050993ff935d882423307a47c0161b9e06c
Antonette Caldwell <acald.nebula@gmail.com>
2023-11-01 22:22:53 -0500

    feat: init style dictionary basic

---
+++ packages/css-library/build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Thu, 02 Nov 2023 03:20:07 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
